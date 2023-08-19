35fcd36699f9b582947f1178b415388aec1d2d81
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-19 09:43:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const maxSideLenN = (m, n, r, c) => {
+  // for len s, you need:
+
+  // room to the left
+  // c - (s - 1) >= 0
+  // c - s + 1 >= 0
+  // s <= c + 1
+
+  // room to the right
+  // c + (s - 1) <= n - 1
+  // c + s <= n
+  // s <= n - c
+
+  // room below
+  // r + 2 * (s - 1) <= m - 1
+  // 2 * (s - 1) <= m - 1 - r
+  // 2s - 2 <= m - 1 - r
+  // 2s <= m + 1 - r
+  // s <= ceil((m - r + 1) / 2)
+
+  return Math.min(c + 1, n - c, Math.floor((m - r + 1) / 2));
+};
+
+const sideSum = (grid, R, C, r, c) => {
+  const sideSums = {};
+  const sideSumWMemo = (R, C, r, c) => {
+    if (sideSums[`${R}#${C}#${r}#${c}`]) {
+      // console.log(`returning sideSums[${R}#${C}#${r}#${c}]`)
+      return sideSums[`${R}#${C}#${r}#${c}`];
+    }
+    // base case
+    if (R === r && C === c) {
+      sideSums[`${R}#${C}#${r}#${c}`] = grid[R][C];
+      return grid[R][C];
+    }
+    // recursive case
+    const goWest = c > C;
+    const goEast = !goWest;
+    if (goWest) {
+      sideSums[`${R}#${C}#${r}#${c}`] =
+        grid[r][c] + sideSumWMemo(R, C, r - 1, c - 1);
+      return sideSums[`${R}#${C}#${r}#${c}`];
+    }
+    if (goEast) {
+      console.log("going east")
+      sideSums[`${R}#${C}#${r}#${c}`] =
+        grid[r][c] + sideSumWMemo(R, C, r - 1, c + 1);
+      return sideSums[`${R}#${C}#${r}#${c}`];
+    }
+  };
+  return sideSumWMemo(R, C, r, c);
+};
+
+module.exports = { maxSideLenN, sideSum };
---
+++ index.test.js
@@ -0,0 +1,88 @@
+const maxSideLenN = require(".").maxSideLenN;
+const sideSum = require(".").sideSum;
+describe("rhombus", () => {
+  describe("maxSideLenN", () => {
+    test.each([
+      {
+        m: 8,
+        n: 8,
+        r: 1,
+        c: 3,
+        expected: 4,
+      },
+      {
+        m: 8,
+        n: 8,
+        r: 7,
+        c: 7,
+        expected: 1,
+      },
+      {
+        m: 1,
+        n: 1,
+        r: 0,
+        c: 0,
+        expected: 1,
+      },
+      {
+        m: 8,
+        n: 8,
+        r: 1,
+        c: 5,
+        expected: 3,
+      },
+      {
+        m: 3,
+        n: 3,
+        r: 1,
+        c: 1,
+        expected: 1,
+      },
+    ])("$m, $n, $r, $c -> $expected", ({ m, n, r, c, expected }) => {
+      expect(maxSideLenN(m, n, r, c)).toEqual(expected);
+      expect(true).toBe(true);
+    });
+  });
+  describe("sideSum", () => {
+    test.each([
+      {
+        description: "1 cell",
+        grid: [[1]],
+        R: 0,
+        C: 0,
+        r: 0,
+        c: 0,
+        expected: 1,
+      },
+      {
+        description: "base case of 'side length 2'",
+        grid: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        R: 0,
+        C: 1,
+        r: 0,
+        c: 1,
+        expected: 2,
+      },
+      {
+        description: "side length 2",
+        grid: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        R: 0,
+        C: 1,
+        r: 1,
+        c: 0,
+        expected: 6,
+      },
+      {
+        description: "Longside 1",
+        grid: [[3,4,5,1,3],[3,3,4,2,3],[20,30,200,40,10],[1,5,5,4,1],[4,3,2,2,5]],
+        R: 0,
+        C: 3,
+        r: 3,
+        c: 0,
+        expected: 36,
+      },
+    ])("$description", ({ grid, R, C, r, c, expected }) => {
+      expect(sideSum(grid, R, C, r, c)).toEqual(expected);
+    });
+  });
+});
